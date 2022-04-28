366b7ffb3d45ccc6a6a9d32c892b6772b0e65214
Jegor <jegor.pakhutko@gmail.com>
2022-04-28 23:52:05 +0300

    task2

---
+++ src/com/max/idea/Main.java
@@ -0,0 +1,10 @@
+package com.max.idea;
+
+public class Main {
+    public static void main (String[] args) {
+        double big1 = 3.1416;
+        short small1 = -32768;
+        System.out.printf("%.4f\n", big1);
+        System.out.printf("%d\n", small1);
+    }
+}
